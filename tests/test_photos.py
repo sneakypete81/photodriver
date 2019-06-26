d6931dbb2a8da7f41a9dd98174a977096a0b17fe
sneakypete81 <sneakypete81@gmail.com>
2019-06-26 21:31:00 +0100

    Basic date range test

--- tests/test_photos.py
+++ tests/test_photos.py
@@ -1,4 +1,5 @@
 from datetime import date
+
 # from hamcrest import assert_that, is_
 import pytest
 from unittest.mock import Mock, patch
@@ -8,19 +9,23 @@ from photodriver.driver import Driver
 from photodriver.photos import Photos
 
 
+def CheckboxMock(date):
+    return Mock(Checkbox, element=Mock(), label=f"checkbox {date}", date=date)
+
+
 @pytest.fixture
 def checkboxes():
     return [
-        Mock(Checkbox, element=Mock(), label="checkbox1", date=date(2019, 11, 5)),
-        Mock(Checkbox, element=Mock(), label="checkbox2", date=date(2019, 11, 4)),
-        Mock(Checkbox, element=Mock(), label="checkbox3", date=date(2019, 11, 3)),
-        Mock(Checkbox, element=Mock(), label="checkbox4", date=date(2019, 11, 2)),
-        Mock(Checkbox, element=Mock(), label="checkbox5", date=date(2019, 11, 1)),
+        CheckboxMock(date(2019, 11, 5)),
+        CheckboxMock(date(2019, 11, 4)),
+        CheckboxMock(date(2019, 11, 3)),
+        CheckboxMock(date(2019, 11, 2)),
+        CheckboxMock(date(2019, 11, 1)),
     ]
 
 
+@patch("photodriver.photos.PhotoScroller", autospec=True, spec_set=True)
 class TestSelectRange:
-    @patch("photodriver.photos.PhotoScroller", autospec=True, spec_set=True)
     def test_all_photos_are_selected_if_dates_are_unspecified(
         self, PhotoScroller, checkboxes
     ):
@@ -34,3 +39,21 @@ class TestSelectRange:
 
         checkboxes[-1].click.assert_called()
         checkboxes[0].shift_click.assert_called()
+
+    def test_photos_are_selected_between_two_dates(self, PhotoScroller, checkboxes):
+        driver = Mock(Driver)
+        PhotoScroller(driver).get_visible_checkboxes.return_value = checkboxes
+        PhotoScroller(driver).to_top.return_value = checkboxes[0]
+        PhotoScroller(driver).to_bottom.return_value = checkboxes[-1]
+
+        def find_last_matching_checkbox(date):
+            matches = [checkbox for checkbox in checkboxes if checkbox.date == date]
+            return matches[-1]
+
+        PhotoScroller(driver).up_to_checkbox.side_effect = find_last_matching_checkbox
+
+        photos = Photos(driver)
+        photos.select_range(date(2019, 11, 2), date(2019, 11, 4))
+
+        checkboxes[3].click.assert_called()
+        checkboxes[2].shift_click.assert_called()
