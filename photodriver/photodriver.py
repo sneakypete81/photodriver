a2c6fb732ac372e1ecfc5983f30478acaa87b1b5
sneakypete81 <sneakypete81@gmail.com>
2019-06-30 15:19:03 +0100

    Allow cookie file to be specified

--- photodriver/photodriver.py
+++ photodriver/photodriver.py
@@ -1,16 +1,19 @@
 from .driver import Driver
 from .photos import Photos
 
-COOKIE_FILE = ".cookies"
+DEFAULT_COOKIE_FILE = ".cookies"
 
 
-def photodriver(start_date, stop_date):
+def photodriver(start_date, stop_date, cookie_file=None):
+    if cookie_file is None:
+        cookie_file = DEFAULT_COOKIE_FILE
+
     driver = Driver()
     try:
         photos = Photos(driver)
-        photos.load_cookies(COOKIE_FILE)
+        photos.load_cookies(cookie_file)
         photos.login()
-        photos.save_cookies(COOKIE_FILE)
+        photos.save_cookies(cookie_file)
 
         count = photos.select_range(start_date, stop_date)
         print(f"Downloaded {count} photos.")
