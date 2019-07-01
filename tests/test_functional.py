e8052ca2fc65c8dd6bb3847a3f4b59f9040ba3d4
sneakypete81 <sneakypete81@gmail.com>
2019-07-01 13:57:23 +0100

    Use a single driver for functional tests, logged in automatically

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,8 +1,35 @@
+from pathlib import Path
+
+from hamcrest import assert_that, only_contains, has_properties, has_length
 import pytest
 
 import photodriver
+from photodriver.driver import Driver
+from photodriver.photos import Photos
 
 TEST_COOKIE_FILE = ".test-cookies"
+
+PASSWORD = (Path(__file__).parent / ".functional.password").read_text().strip()
+
+
+@pytest.fixture(scope="module")
+def logged_in_driver():
+    """Create a single driver object to be used for all tests in the module."""
+    driver = Driver()
+    driver_close = driver.close
+    driver.close = lambda: None
+    try:
+        photos = Photos(driver)
+        photos.login("photodriver.test@gmail.com", PASSWORD)
+        yield driver
+    finally:
+        driver_close()
+
+
+@pytest.fixture()
+def patched_driver(logged_in_driver, monkeypatch):
+    """Monkeypatch the Driver class to use our test driver."""
+    monkeypatch.setattr(photodriver.photodriver, "Driver", lambda: logged_in_driver)
 
 
 @pytest.mark.functional
